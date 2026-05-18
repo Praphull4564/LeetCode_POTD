6dde17674d35e073f32e3cdb3133697523f9c6fa
Praphull4564 <praphullky4564@gmail.com>
2026-05-18 20:09:09 +0530

    Time: 118 ms (97.62%), Space: 32.7 MB (98.94%) - LeetHub

---
+++ 1345-jump-game-iv/1345-jump-game-iv.py
@@ -0,0 +1,31 @@
+class Solution:
+    def minJumps(self, arr: List[int]) -> int:
+        n = len(arr)
+        if n == 1:
+            return 0
+        graph = defaultdict(list)
+        for i, val in enumerate(arr):
+            graph[val].append(i)
+        q = deque([0])
+        visited = [False] * n
+        visited[0] = True
+        cnt = 0
+        while q:
+            for _ in range(len(q)):
+                idx = q.popleft()
+                if idx == n - 1:
+                    return cnt
+                if idx - 1 >= 0 and not visited[idx - 1]:
+                    visited[idx - 1] = True
+                    q.append(idx - 1)
+                if idx + 1 < n and not visited[idx + 1]:
+                    visited[idx + 1] = True
+                    q.append(idx + 1)
+                if arr[idx] in graph:
+                    for nxt in graph[arr[idx]]:
+                        if not visited[nxt]:
+                            visited[nxt] = True
+                            q.append(nxt)
+                    del graph[arr[idx]]
+            cnt += 1
+        return -1
