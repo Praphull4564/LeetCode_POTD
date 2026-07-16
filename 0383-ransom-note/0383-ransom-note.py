bc7a5fbb0de2f53e1f9990aec8b9be7e68d6f49a
Praphull Kumar Yadav <praphullky4564@gmail.com>
2026-07-16 17:58:13 +0530

    Time: 26 ms (37.22%), Space: 19.6 MB (71.87%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,22 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        d1,d2={},{}
+
+        for i in ransomNote:
+            if i in d1:
+                d1[i]+=1
+            else:
+                d1[i]=1
+        for i in magazine:
+            if i in d2:
+                d2[i]+=1
+            else:
+                d2[i]=1
+
+        for i in d1:
+            if i in d2 and d1[i]<=d2[i]:
+                continue
+            else:
+                return False
+        else:
+            return True
