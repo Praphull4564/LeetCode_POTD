996dfac12e77abb3ded2478c9e0a49e6cf1111fa
Praphull Kumar Yadav <praphullky4564@gmail.com>
2026-08-04 12:48:16 +0530

    Time: 2 ms (75.11%), Space: 19.3 MB (55.74%) - LeetHub

---
+++ 3731-find-missing-elements/3731-find-missing-elements.py
@@ -0,0 +1,5 @@
+class Solution:
+    def findMissingElements(self, nums: List[int]) -> List[int]:
+        l=set([i for i in range(min(nums),max(nums)+1)])
+        x=set(nums)
+        return sorted(list(l - x))
