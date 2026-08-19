63eee6437845ac253be0f1444676702471cdbef6
Praphull Kumar Yadav <praphullky4564@gmail.com>
2026-08-19 22:22:53 +0530

    Time: 49 ms (43.7%), Space: 23.7 MB (7.97%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.py
@@ -1,19 +1,22 @@
 class Solution:
-    def maxNumberOfFamilies(
-        self, n: int, reservedSeats: List[List[int]]
-    ) -> int:
-        left, middle, right = 0b11110000, 0b11000011, 0b00001111
-        occupied = collections.defaultdict(int)
-        for seat in reservedSeats:
-            if 2 <= seat[1] <= 9:
-                occupied[seat[0]] |= 1 << (seat[1] - 2)
-
-        ans = (n - len(occupied)) * 2
-        for row, bitmask in occupied.items():
-            if (
-                (bitmask | left) == left
-                or (bitmask | middle) == middle
-                or (bitmask | right) == right
-            ):
-                ans += 1
-        return ans
+    def maxNumberOfFamilies(self, n: int, rS: List[List[int]]) -> int:
+        d={}
+        for i in rS:
+            if i[0] in d:
+                d[i[0]].add(i[1])
+            else:
+                d[i[0]]=set([i[1]])
+        res=2*(n-len(d))
+        for i in d:
+            l=d[i]
+            t2345=False
+            if 2 not in l and 3 not in l and 4 not in l and 5 not in l:
+                res+=1
+                t2345=True
+            t4567=False
+            if 7 not in l and 6 not in l and 4 not in l and 5 not in l and not t2345:
+                res+=1
+                t4567=True
+            if 7 not in l and 6 not in l and 8 not in l and 9 not in l and not t4567:
+                res+=1
+        return res
